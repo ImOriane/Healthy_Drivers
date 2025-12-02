142a3878912b79195d4e2d66271ea80b823cfed8
ImOriane <oriane.foy@gmail.com>
2025-12-02 15:20:17 +0000

    abonnement

--- resources/views/abonnements.blade.php
+++ resources/views/abonnements.blade.php
@@ -1,4 +1,83 @@
-
 <x-app-layout>
+    <div class="bg-[#EFE0D2] h-screen flex flex-col">
 
+        <section class="w-full bg-[#E67E22] pt-16 pb-12 mt-16"> <div class="max-w-7xl mx-auto"> <h1 class="text-4xl md:text-5xl font-bold text-white">ABONNEMENTS</h1> </div> </section>
+
+        <section class="flex-grow container mx-auto px-6 py-6 flex flex-wrap justify-center gap-36 items-center">
+
+            <div class="bg-white max-w-[240px] shadow-lg relative flex flex-col min-h-[390px]">
+                <div class="h-4 bg-[#9CD83C]"></div>
+                <div class="p-6 flex flex-col justify-between">
+
+                    <div class="flex flex-col mb-6 text-center">
+                        <h2 class="text-[#365413] font-bold uppercase text-lg mb-2 ">Économique</h2>
+                        <p class="text-3xl font-extrabold mb-2">€47.99 <span class="text-base font-normal text-gray-600">/mois</span></p>
+                        <p class="bg-[#FFD2B3] text-[#F37021] text-xs font-semibold uppercase inline-block py-1 px-3 rounded">
+                            7.99€/repas
+                        </p>
+                    </div>
+
+                    <ul class="text-[#365413]  text-sm space-y-2 mb-6">
+                        <li>🥕 6 repas par semaine</li>
+                        <li>🥕 Mise en relation avec coach nutritionnel</li>
+                        <li>🥕 Accès au recettes proposées sur le site web</li>
+                    </ul>
+
+                    <button class="w-full bg-[#F37021] text-white font-bold py-2 hover:bg-[#d75a00] transition">
+                        Choisir
+                    </button>
+                </div>
+            </div>
+
+            <div class="bg-white max-w-[240px] shadow-lg relative flex flex-col min-h-[390px]">
+                <div class="h-4 bg-[#9CD83C]"></div>
+                <div class="p-6 flex flex-col justify-between">
+
+                    <div class="flex flex-col mb-6 text-center">
+                        <h2 class="text-[#365413] font-bold uppercase text-lg mb-2">Classique</h2>
+                        <p class="text-3xl font-extrabold mb-2">€69.99 <span class="text-base font-normal text-gray-600">/mois</span></p>
+                        <p class="bg-[#FFD2B3] text-[#F37021] text-xs font-semibold uppercase inline-block py-1 px-3 rounded">
+                            6,99€/repas
+                        </p>
+                    </div>
+
+                    <ul class="text-[#365413] text-sm space-y-2 mb-6">
+                        <li>🥕 10 repas par semaine</li>
+                        <li>🥕 Mise en relation avec coach nutritionnel</li>
+                        <li>🥕 Accès recettes adaptées à la continuité de votre régime</li>
+                    </ul>
+
+                    <button class="w-full bg-[#F37021] text-white font-bold py-2 hover:bg-[#d75a00] transition">
+                        Choisir
+                    </button>
+                </div>
+            </div>
+
+            <div class="bg-white max-w-[240px] shadow-lg relative flex flex-col min-h-[390px]">
+                <div class="h-4 bg-[#9CD83C]"></div>
+                <div class="p-6 flex flex-col justify-between">
+
+                    <div class="flex flex-col mb-6 text-center">
+                        <h2 class="text-[#365413] font-bold uppercase text-lg mb-2">Premium</h2>
+                        <p class="text-3xl font-extrabold mb-2">€89.99 <span class="text-base font-normal text-gray-600">/mois</span></p>
+                        <p class="bg-[#FFD2B3] text-[#F37021] text-xs font-semibold uppercase inline-block py-1 px-3 rounded">
+                            5,71€/repas
+                        </p>
+                    </div>
+
+                    <ul class="text-[#365413]  text-sm space-y-2 mb-6">
+                        <li>🥕 14 repas par semaine</li>
+                        <li>🥕 Mise en relation avec coach nutritionnel</li>
+                        <li>🥕 Accès recettes adaptées à la continuité de votre régime</li>
+                    </ul>
+
+                    <button class="w-full bg-[#F37021] text-white font-bold py-2 hover:bg-[#d75a00] transition">
+                        Choisir
+                    </button>
+                </div>
+            </div>
+
+        </section>
+
+    </div>
 </x-app-layout>
