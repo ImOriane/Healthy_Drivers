f5d8214d3a1156523ac5f320549b75df4551c383
axel <axelalmeidarosa@gmail.com>
2026-03-09 16:41:54 +0000

    reservationController.php

--- app/Http/Controllers/Reservation/reservationController.php
+++ app/Http/Controllers/Reservation/reservationController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Idea;
 
-class reservationController extends Controller
+class ReservationController extends Controller
 {
+    //
 }
